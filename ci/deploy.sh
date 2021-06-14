141a737900ae22ae0e7deef08f9fccf47395f4ef
panzuhong <panzuhong@shoplazza.com>
2021-06-14 21:40:50 +0800

    fix

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,3 +1,3 @@
 echo 'Deploying...'
-rsync -rv --delete -e 'ssh -o stricthostkeychecking=no' dist/ root@121.89.233.173:/usr/local/src/v3_demo
+rsync -rv --delete -e 'ssh -o stricthostkeychecking=no' dist root@121.89.233.173:/usr/local/src/v3_demo
 echo 'SUCCESS!'
