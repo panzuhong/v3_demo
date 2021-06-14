e00f253f615674fc250223e30d6700f422f7c365
panzuhong <panzuhong@shoplazza.com>
2021-06-14 21:54:38 +0800

    d

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,4 +1,4 @@
 echo 'Deploying...'
 npm run build
-rsync -rv --delete -e 'ssh -o stricthostkeychecking=no' dist root@121.89.233.173:/usr/local/src/v3_demo
+rsync -rv --delete -e 'ssh -o stricthostkeychecking=no' dist/ root@121.89.233.173:/usr/local/src/v3_demo
 echo 'SUCCESS!'
